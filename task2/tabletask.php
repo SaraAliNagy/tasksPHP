be2ca927561788293b07822ed58aa058e9d7d278
SaraAliNagy <saranagy47@gmail.com>
2022-01-07 04:08:21 +0200

    task 2 updated

--- task2/tabletask.php
+++ task2/tabletask.php
@@ -12,9 +12,7 @@ $users = [
         'id' => 1,
         'name' => 'ahmed',
         "gender" => (object)[
-            'gender' => 'm' 
-
-        ,    
+            'gender' => 'm',
         ],
         'hobbies' => [
             'football', 'swimming', 'running'
@@ -22,7 +20,8 @@ $users = [
         'activities' => [
             "school" => 'drawing',
             'home' => 'painting'
-        ]
+        ] ,
+        'email' => 'ahmed@gmail.com'
     ],
     (object)[
         'id' => 2,
@@ -36,7 +35,8 @@ $users = [
         'activities' => [
             "school" => 'painting',
             'home' => 'drawing'
-        ]
+        ],
+        'email' => 'mohamed@gmail.com'
     ],
     (object)[
         'id' => 3,
@@ -50,48 +50,84 @@ $users = [
         'activities' => [
             "school" => 'painting',
             'home' => 'drawing'
-        ]
+        ],
+        'email' => 'menna@gmail.com'
     ],
-    
+
+    (object)[
+        'id' => 3,
+        'name' => 'menna',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ]
+        ,
+        'email' => ''
+    ]
+
 ];
 
+// users === tr
+// user properties === th
+// user values === td
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Title</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-    <table class="table table-striped">
+</head>
+
+<body>
+    <table class="table table-success table-striped">
         <thead>
-            <?php 
-                foreach ($users as $index => $user) { 
-                        foreach ($user   as $index => $information) {
-                            echo "<th>$index </th>" ;
-                        }
-                        break;
-                }
+            <?php
+            foreach ($users[0] as $index => $value) {
             ?>
+                <th class="table-dark table-active"><?php echo $index  ?></th>
+
+            <?php } ?>
         </thead>
         <tbody>
-                <?php 
-                        
-                    foreach ($users as $index => $user) { 
-                            foreach ($user   as $index => $info) {
-                                echo "<td> $info </td>" ;    
-
-                    }
-                }
-                
-                ?>
-                
-               
+            <?php
+            foreach ($users as $index => $user) {
+            ?>
+                <tr>
+                    <?php
+                    foreach ($user as $property => $value) { ?>
+                        <td>
+                            <?php
+                            if (gettype($value) == 'array' || gettype($value) == 'object') {
+                                foreach ($value as $k => $v) {
+                                    if ($property == 'gender') {
+                                        if ($v == 'm') {
+                                            $v = 'male';
+                                        } else {
+                                            $v = 'female';
+                                        }
+                                    } 
+                                        echo $v . " , ";
+                                }
+                            } else {
+                                echo $value;
+                            }
+                            ?>
+                        </td>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
 
@@ -111,5 +147,6 @@ $users = [
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
